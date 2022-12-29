b836d12af564ab760f7191a3c9709afe0528c33d
DESKTOP-FK89NF0\hgh04 <gbwebdeveloper0425@gmail.com>
2022-12-29 22:04:50 +0900

    프로젝트 생성

---
+++ bordsite/src/main/java/GBweb/bordsite/BordsiteApplication.java
@@ -0,0 +1,13 @@
+package GBweb.bordsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BordsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BordsiteApplication.class, args);
+	}
+
+}
---
+++ bordsite/src/test/java/GBweb/bordsite/BordsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package GBweb.bordsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BordsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
