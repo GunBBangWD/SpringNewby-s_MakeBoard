c65154a8f94b911ea01ee50f5c142a25d1454476
DESKTOP-FK89NF0\hgh04 <gbwebdeveloper0425@gmail.com>
2022-12-31 07:11:07 +0900

    리포지토리 단위테스트완

--- bordsite/src/test/java/GBweb/bordsite/Repository/MemoryNoticeRepositoryTest.java
+++ bordsite/src/test/java/GBweb/bordsite/Repository/MemoryNoticeRepositoryTest.java
@@ -1,4 +1,29 @@
 package GBweb.bordsite.Repository;
 
+import GBweb.bordsite.domain.Notice;
+import GBweb.bordsite.repository.MemoryNoticeRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
 public class MemoryNoticeRepositoryTest {
+    MemoryNoticeRepository repository = new MemoryNoticeRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Notice notice = new Notice();
+        notice.setName("spring");
+
+        repository.save(notice);
+        Notice result = repository.findById(notice.getId()).get();
+        Assertions.assertThat(notice).isEqualTo(result);
+
+    }
+
+
 }
